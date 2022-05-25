814b4c091e32a5e5fd4f71f0f4f42223298a04df
sheilaflood <floodshe1@gmail.com>
2022-05-24 18:49:16 -0700

    adding model.py test file

---
+++ modeling/model.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+path = '/Users/sheilaflood/SWB-GVCEH/scraper/data/'
+files = os.listdir(path)
+last_file_path = path + files[-1]
+df = pd.read_csv(last_file_path)
+
+new_file_path = last_file_path.replace('scraper', 'modeling').replace('raw.csv', 'scored.csv')
+df.to_csv(new_file_path)
+print('done!')
