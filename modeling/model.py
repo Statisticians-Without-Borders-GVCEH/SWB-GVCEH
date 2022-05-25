03505f940172c7bc58d952f406679ad19f9017d3
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-05-24 22:13:23 -0700

    Update model.py

--- modeling/model.py
+++ modeling/model.py
@@ -1,10 +1,14 @@
 import os
 import pandas as pd
 
-path = '../scraper/data/'
+# path = '../scraper/data/'
+path = os.path.dirname(os.path.abspath(__file__))
+path = path.replace('modeling', 'scraper/data/')
+print(path)
 files = os.listdir(path)
 last_file_path = path + files[-1]
 df = pd.read_csv(last_file_path)
+
 
 new_file_path = last_file_path.replace('scraper', 'modeling').replace('raw.csv', 'scored.csv')
 df.to_csv(new_file_path)
