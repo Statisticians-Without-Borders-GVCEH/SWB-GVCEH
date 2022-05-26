efdc47ff0a1ab93e68cbe9b6ac1d67128c750b76
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-05-25 17:07:39 -0700

    Update model.py
    uses api

--- modeling/model.py
+++ modeling/model.py
@@ -1,30 +1,40 @@
 import pandas as pd
 import requests
 import io
+import os
+from dotenv import load_dotenv
+from github import Github
 
-# ssl._create_default_https_context = ssl._create_unverified_context
+load_dotenv() #take environment variables from .env
 
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
+GITHUB_REPO = 'SWB-GVCEH'
 
-github_session = requests.Session()
-github_session.auth = (username, token)
+g = Github(USERNAME, TOKEN)
+user = g.get_user(USERNAME)
+repo = user.get_repo('SWB-GVCEH')
+# print(repo.full_name)
 
+my_repo = g.get_repo(repo.full_name)
+contents = my_repo.get_contents("")
+all_files = []
+while contents:
+    file_content = contents.pop(0)
+    if file_content.type == "dir":
+        contents.extend(repo.get_contents(file_content.path))
+    else:
+        file = file_content
+        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
 
-url = 'https://raw.githubusercontent.com/sheilaflood/SWB-GVCEH/main/scraper/data/'
-file = 'GVCEH-2022-04-10-tweet-raw.csv'
-old_url = url + file
+# Read from github
+with open('../scraper/data/GVCEH-2022-05-23-tweet-raw.csv', 'r') as file:
+    content = file.read()
 
-download = github_session.get(old_url).content
-df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-print(df.head())
-
-
-new_url = url.replace('scraper', 'modeling')
-new_file = file.replace('raw.csv', 'scored.csv')
-print(new_url + new_file)
-
-github_session.post(new_url, data = df.to_csv(new_file))
-
-print('Done!')
+# upload to github
+git_file = 'modeling/data/GVCEH-2022-05-23-tweet-raw-duplicate.csv'
+repo.create_file(git_file, "committing new file", content, branch="main")
+print('done!!!')
 
 
 
