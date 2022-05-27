7bf87f464c1de2d4fd4b15c9274dae44e035a244
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-05-26 19:24:34 -0700

    Update model.py

--- modeling/model.py
+++ modeling/model.py
@@ -2,20 +2,34 @@ import pandas as pd
 import requests
 import io
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from github import Github
+import csv
+import tensorflow
+from transformers import pipeline
 
-# load_dotenv() #take environment variables from .env
 
-USERNAME = os.environ["USERNAME"]
-TOKEN = os.environ["TOKEN"]
+load_dotenv() #take environment variables from .env
 
+# create the model
+model = pipeline("sentiment-analysis",
+                 model='cardiffnlp/twitter-roberta-base-sentiment-latest',
+                 device=0
+                 )
+model.tokenizer.model_max_length = 512
+
+
+# load environment variables
+USERNAME = os.getenv["USERNAME"]
+TOKEN = os.getenv["TOKEN"]
+
+# open github api connection
 g = Github(USERNAME, TOKEN)
 user = g.get_user(USERNAME)
 repo = user.get_repo('SWB-GVCEH')
-# print(repo.full_name)
-
 my_repo = g.get_repo(repo.full_name)
+
+
 contents = my_repo.get_contents("")
 all_files = []
 while contents:
@@ -28,12 +42,23 @@ while contents:
 
 # Read from github
 with open('./scraper/data/GVCEH-2022-05-23-tweet-raw.csv', 'r') as file:
-    content = file.read()
+    df = pd.read_csv(file)
+    # print(df.info())
+
+all_res = []
+for res in model(df.text.to_list(), batch_size=32, truncation=True):
+  all_res.append(res)
+
+all_sentiments = [x['label'] for x in all_res]
+all_scores = [x['score'] for x in all_res]
+
+df['sentiment'] = all_sentiments
+df['score'] = all_scores
+df.head()
+
+stuff = df.to_csv()
 
 # upload to github
-git_file = 'modeling/data/GVCEH-2022-05-23-tweet-raw-duplicate.csv'
-repo.create_file(git_file, "committing new file", content, branch="main")
-print('done!!!')
-
-
-
+git_file = 'modeling/data/GVCEH-2022-05-23-tweet-scored.csv'
+repo.create_file(git_file, "committing new file", stuff, branch="main")
+print('Done!!!')
