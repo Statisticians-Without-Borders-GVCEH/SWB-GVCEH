cc5126f8669ab74d49f337ad11b5703c202d1c5e
sheilaflood <floodshe1@gmail.com>
2022-07-12 20:12:28 -0700

    Cleaned up dashboard

--- dashboard/gvceh_functions.py
+++ dashboard/gvceh_functions.py
@@ -4,27 +4,28 @@ import pandas as pd
 import numpy as np
 
 def get_seed():
-	''' Input the influencer file and add a boolean influencer column to the dataframes.
-	Specify the twitter source in src.'''
+    ''' Input the influencer file and add a boolean influencer column to the dataframes.
+    Specify the twitter source in src.'''
 
-	reddit_df = pd.read_csv('./data/processed/reddit_antiwork.csv',
-		parse_dates=['timestamp'], dtype={'created': object, 'score': float})
+    # reddit_df = pd.read_csv('./data/processed/reddit_antiwork.csv',
+    # 	parse_dates=['timestamp'], dtype={'created': object, 'score': float})
 
-	twitter_df = pd.read_csv('./data/processed/demo/GVCEH-2022-04-11-tweet-raw-sentiment.csv',
-    	parse_dates=['created_at'], dtype={'tweet_id': object})
+    twitter_df = pd.read_csv('./data/all-raw-merged-2022-06-22_DEMO.csv',
+        parse_dates=['created_at'], dtype={'tweet_id': object})
 
-	# next two lines should be part of post scrape clean-up
-	influencer_list = list(pd.read_csv('./dashboard/influencers.csv')['handle'])
+    # next two lines should be part of post scrape clean-up
+    influencer_list = list(pd.read_csv('./dashboard/influencers.csv')['handle'])
 
-	twitter_df['influencer_flag'] = twitter_df['username'].apply(lambda x: 1 if x in influencer_list else 0)
+    twitter_df['influencer_flag'] = twitter_df['username'].apply(lambda x: 1 if x in influencer_list else 0)
 
-	return twitter_df, reddit_df
+    return twitter_df#, reddit_df
 
 
 def get_data():
     ''' Loading the twitter and reddit datasets and generating a dictionary of dataframes.'''
 
-    twitter_df, reddit_df = get_seed()
+    # twitter_df, reddit_df = get_seed()
+    twitter_df = get_seed()
 
     # reddit_df = pd.read_csv('./twitter/demo/reddit_antiwork.csv',
     #  parse_dates=['timestamp'], dtype={'created': object, 'score': float})
@@ -32,7 +33,7 @@ def get_data():
     # twitter_df = pd.read_csv('./twitter/demo/GVCEH-2022-04-11-tweet-raw-sentiment.csv',
     # 	parse_dates=['created_at'], dtype={'tweet_id': object})
 
-    return {'Twitter': twitter_df, 'Reddit': reddit_df}
+    return {'Twitter': twitter_df}#, 'Reddit': reddit_df}
 
 
 def tooltips():
@@ -66,14 +67,9 @@ def get_frames(start, end, df):
     current = df.loc[(df.created_at >= datetime.combine(start, time())) &
                      (df.created_at <= datetime.combine(end, time()))]
 
-    # current =  df.loc[(df.created_at >= pd.Timestamp(start)) &
-    # (df.created_at <= pd.Timestamp(end))]
-
     prior = df.loc[(df.created_at >= datetime.combine(prev_start, time())) &
                    (df.created_at <= datetime.combine(prev_end, time()))]
 
-    # prior = df.loc[(df.created_at >= pd.Timestamp(prev_start)) &
-    # (df.created_at <= pd.Timestamp(prev_end))]
 
     return current, prior
 
@@ -92,50 +88,20 @@ def agg_sentiments_by_category(cdf, pdf):
 
     return by_category
 
-# def weighted_agg_sentiments_by_category(cdf, pdf, in_wt):
-# 	''' Adding weights separating influencer and non-influencer tweets.'''
 
-# 	cagg = pd.DataFrame(cdf.groupby(['influencer_flag'], as_index=False)['sentiment'].value_counts())
-# 	cagg.columns = ['influencer_flag', 'Sentiment', 'cur']
-
-# 	pagg = pd.DataFrame(pdf.groupby(['influencer_flag'], as_index=False)['sentiment'].value_counts())
-# 	pagg.columns = ['influencer_flag', 'Sentiment', 'pre']
-
-# 	cagg['cur_scores'] = cagg.apply(lambda x: in_wt * x.cur if x.influencer_flag == True else (1 - in_wt) * x.cur, axis=1)
-# 	cagg_by_category =  pd.DataFrame(cagg.groupby(['Sentiment'])['cur_scores'].sum())
-
-# 	pagg['pre_scores'] = pagg.apply(lambda x: in_wt * x.pre if x.influencer_flag == True else (1 - in_wt) * x.pre, axis=1)
-
-# 	by_category =  cagg.merge(pagg, how='left').reset_index(drop=True)
-
-
-# 	return by_category
 
 def top_influencers(cdf):
 
-	current_influencers = pd.DataFrame(cdf.groupby(['username']).agg({'tweet_id':'count', 'reply_count':'sum', 
+    current_influencers = pd.DataFrame(cdf.groupby(['username']).agg({'tweet_id':'count', 'reply_count':'sum',
 		'retweet_count':'sum', 'like_count':'sum', 'influencer_flag':'min', 'num_followers':'max'}).reset_index())
 
-	current_influencers['score'] = current_influencers.apply(lambda x: x.tweet_id * 0.1 + 
+    current_influencers['score'] = current_influencers.apply(lambda x: x.tweet_id * 0.1 +
 		(x.reply_count + x.retweet_count) * 0.25 + x.like_count * 0.05 + 
 		x.influencer_flag * 0.45 + np.log(x.num_followers) * 0.15, axis=1)
 
-	current_influencers = current_influencers.sort_values(by=['score'], ascending=False).reset_index(drop=True)
-	# current_influencers.columns = ['username', 'Number of Tweets']
+    current_influencers = current_influencers.sort_values(by=['score'], ascending=False).reset_index(drop=True)
 
-	# cnum = pd.DataFrame(current_df.groupby(['username']).max()['num_followers']).reset_index()
-	# current_influencers = current_influencers.merge(cnum, left_on='username',
-	# 	right_on='username', how='left')
-	# current_influencers.columns = ['Username', 'Number of Tweets', 'Number of Followers']
-
-	# st.table(current_influencers.iloc[0:5])
-	# fig_3 = px.bar(current_influencers.iloc[0:5], x='Username', y='Number of Tweets', color_discrete_sequence=['#000080'])
-	# st.plotly_chart(fig_3)
-
-    # display tweets by top influencer ka option do - button
-    # number of tweets in current+previous period by day or, so far ka just one chart
-
-	return current_influencers
+    return current_influencers
 
 
 def get_appendix_a_locations():
--- dashboard/mockup.py
+++ dashboard/mockup.py
@@ -9,11 +9,6 @@ import numpy as np
 import gvceh_functions as gvceh
 from PIL import Image
 
-# import sharepoint_conn as sharep
-
-# testing sharepoint_conn file
-# test_df = sharep.get_data_sharepoint()
-# print(test_df.head())
 
 st.set_page_config(layout="wide")
 
@@ -44,34 +39,39 @@ readme = gvceh.tooltips()
 
 with sidebar:
 
-	st.sidebar.header('1. Data')
-	option = st.sidebar.selectbox('Select a twitter source',
-		('Twitter', 'Reddit'),
-		key='saanich', help=readme['saanich'])
+	# st.sidebar.header('1. Data')
+	# option = st.sidebar.selectbox('Select a data source',
+	# 	('Twitter', 'Reddit'),
+	# 	key='saanich',
+	# 	help=readme['saanich'])
+	option = 'Twitter'
 
 
-	st.sidebar.header('2. Date Range')
+	st.sidebar.header('1. Date Range')
 	scol1, scol2 = st.sidebar.columns(2)
 
 	start_date = scol1.date_input(
 	            "Start Date",
-	            date(2022, 4, 8),
+	            date(2022, 4, 8), #TODO: Make dynamic based on consolidated file dates
 	            # min_value=datetime.strptime("2015-02-16", "%Y-%m-%d"),
 	            max_value=datetime.now(),
 	        )
 
 	end_date = scol2.date_input(
 	            "End Date",
-	            date(2022, 4, 11),
+	            date(2022, 4, 11), #TODO: Make dynamic based on consolidated file dates
 	            # min_value=datetime.strptime("2015-02-24", "%Y-%m-%d"),
 	            max_value=datetime.now(),
 	        )
+
+	if start_date > end_date: # TODO: make so error msg only shows on sidebar; not main page.
+	    st.sidebar.error('Error: End date must be on or after the start date.')
 
 	priorperiod_flag = st.sidebar.checkbox(
 	        "Prior period comparison", value=False, help=readme['langford']
 	    )
 
-	st.sidebar.header('3. Locations')
+	st.sidebar.header('2. Locations')
 
 	appendix_a = gvceh.get_appendix_a_locations()
 	appendix_a_categories = appendix_a["Category"].unique()
@@ -85,32 +85,33 @@ with sidebar:
 	if location_option:
 		locations_selected = st.sidebar.multiselect('Select specific location(s):', location_option,
 													default=location_option)
-	
-	st.sidebar.header('4. Display Top Influencer Tweets')
+
+
+	st.sidebar.header('3. Display Top Influencer Tweets')
 	displaytweets_flag = st.sidebar.checkbox('Display tweets by top influencers', value=False)
 
 
-# define variables based on user options
-use_df = dsource_dict[option]
-current_df, prior_df = gvceh.get_frames(start_date, end_date, use_df)
-sentiments_by_category = gvceh.agg_sentiments_by_category(current_df, prior_df)
-image = Image.open('./dashboard/branding.png')
+	# define variables based on user options
+	use_df = dsource_dict[option]
+	current_df, prior_df = gvceh.get_frames(start_date, end_date, use_df)
+	sentiments_by_category = gvceh.agg_sentiments_by_category(current_df, prior_df)
+	image = Image.open('./dashboard/branding.png')
 
-# wt_try = gvceh.weighted_agg_sentiments_by_category(current_df, prior_df, 0.7)
 
-# additions to sidebar after user input
-st.sidebar.header('5. Download Data')
-st.sidebar.download_button(
-	label="Download data as CSV",
-	data=use_df.to_csv().encode('utf-8'),
-	file_name='twitter.csv')
+	# additions to sidebar after user input
+	st.sidebar.header('4. Download Data')
+	st.sidebar.download_button(
+		label="Download data as CSV",
+		data=use_df.to_csv().encode('utf-8'),
+		file_name='twitter.csv')
 
 
 with header:
-	a1, a2 = st.columns([1,1.5])
 
+	a1, a2 = st.columns([2, 1])
 	a1.title('Homelessness in Greater Victoria')
-	a1.markdown('''This dashboard gives a sense of the sentiment around homelessness in the Greater Victoria area.''')
+	a1.markdown('''This dashboard gives a sense of the sentiment around homelessness in the Greater Victoria area. 
+	Data is collected from Twitter daily and stored in GitHub.''')
 
 	### get a better image after fixing the rest of the layout
 	a2.image(image)
@@ -121,147 +122,79 @@ with header:
 		a1.write('')
 		n = n + 1
 
+	a3, a4 = st.columns(2)
 
-	# 1. Graph of tweets per day historically
-	a1.subheader('Tweets Per Day')
+	# 1. Top Influencers
+	a3.subheader('Top Influencers')
+
+	current_influencers = gvceh.top_influencers(current_df)
+	# st.table(current_influencers)
+	if displaytweets_flag:
+		a3.table(current_df.loc[current_df['username'].isin(current_influencers.iloc[0:5]['username'])][
+					 ['username', 'text']])
+
+	fig_3 = px.bar(current_influencers.iloc[0:5], x='username', y='score', color_discrete_sequence=['#000080'])
+	a3.plotly_chart(fig_3)
+
+	# 2. Sentiment Scores Aggregated by Category, with prior period comparison
+	a4.subheader('Sentiment Scores')
+	if priorperiod_flag:
+		prior_start_date, prior_end_date = gvceh.get_prior_period(start_date, end_date)
+
+		with a4:
+			st.write('Prior period is from', prior_start_date, 'to', prior_end_date)
+
+		fig_1 = px.bar(sentiments_by_category, x='Sentiment', y=['Current', 'Prior'],
+						   barmode='group', color_discrete_sequence=['#000080', '#8c9e5e'] * 3)
+		a4.plotly_chart(fig_1)
+	else:
+		fig_2 = px.bar(sentiments_by_category, x='Sentiment', y='Current', color_discrete_sequence=['#000080'] * 3)
+		a4.plotly_chart(fig_2)
+
+
+	# 3. Graph of tweets per day historically
+	st.subheader('Tweets Per Day')
 	tweets_per_day = use_df.groupby([use_df['created_at'].dt.date]).tweet_id.nunique()
 
 	fig_0 = px.line(tweets_per_day, x=tweets_per_day.index, y="tweet_id",
 		labels={"tweet_id":"Number of Tweets", "created_at":"Date"})
 	fig_0.update_traces(line_color='#BF4C41')
-	a1.plotly_chart(fig_0, use_container_width=True)
+	st.plotly_chart(fig_0, use_container_width=True)
 
 
-	# 2. Viewing a random sample of tweets for sentiment categories
-	a2.subheader('Sample of Tweets' if option == 'Twitter' else 'Sample of Posts')
-	choice = a2.selectbox('Choose a sentiment', 
-		['Negative', 'Neutral', 'Positive'])
-	a2.table((current_df.loc[current_df.sentiment == choice].sample(n=5))[['tweet_id', 'username', 'text']])
-
-	# st.write(wt_try)
-	
-with aggregations:
-
-	b1, b2 = st.columns(2)
-	
-	# @st.cache
-	if start_date > end_date:
-	    st.sidebar.error('Error: End date must be on or after the start date.')
-	else:
-		# st.header(option)
-
-		# 3. Sentiment Scores Aggregated by Category, with prior period comparison
-		b1.subheader('Sentiment Scores')
-		if priorperiod_flag:
-			prior_start_date, prior_end_date = gvceh.get_prior_period(start_date, end_date)
-
-			with b1:
-				st.write('Prior period is from', prior_start_date, 'to', prior_end_date)
-
-			fig_1 = px.bar(sentiments_by_category, x='Sentiment', y=['Current', 'Prior'],
-				barmode='group', color_discrete_sequence=['#000080', '#8c9e5e']*3)
-			b1.plotly_chart(fig_1)
-		else:
-			fig_2 = px.bar(sentiments_by_category, x='Sentiment', y='Current', color_discrete_sequence=['#000080']*3)
-			b1.plotly_chart(fig_2)
-		
-
-		# 4. Top Influencers
-		b2.subheader('Top Influencers')
-		
-		current_influencers = gvceh.top_influencers(current_df)
-		# st.table(current_influencers)
-		if displaytweets_flag:
-			b2.table(current_df.loc[current_df['username'].isin(current_influencers.iloc[0:5]['username'])][['username','text']])
-
-		fig_3 = px.bar(current_influencers.iloc[0:5], x='username', y='score', color_discrete_sequence=['#000080'])
-		b2.plotly_chart(fig_3)
+	# 4. Viewing a random sample of tweets for sentiment categories
+	st.subheader('Sample of Tweets' if option == 'Twitter' else 'Sample of Posts')
+	choice = st.selectbox('Choose a sentiment', ['Negative', 'Neutral', 'Positive'])
+	st.table((current_df.loc[current_df.sentiment == choice].sample(n=5))[['username', 'text', 'sentiment', 'keywords', 'search_neighbourhood']])
 
 
-		# 5. Geolocations
-		st.subheader('Locations')
-		st.write('Sentiment by topic location')
+	# 5. Geolocations
+	st.subheader('Locations')
+	st.write('Sentiment by topic location')
 
-		if locations_selected:
-			current_df = current_df[current_df["search_neighbourhood"].isin(locations_selected)]
-			k = current_df[["search_neighbourhood", "sentiment"]]
-			st.write(k.pivot_table(index='search_neighbourhood',
+	if locations_selected:
+		current_df = current_df[current_df["search_neighbourhood"].isin(locations_selected)]
+		k = current_df[["search_neighbourhood", "sentiment"]]
+		st.write(k.pivot_table(index='search_neighbourhood',
 								   columns='sentiment',
 								   aggfunc=len,
 								   fill_value=0))
 
-		elif agg_option == 'Capital Region District (All)':
-			k = current_df[["search_neighbourhood", "sentiment"]]
-			k_pivot = k.pivot_table(index='search_neighbourhood',
+	elif agg_option == 'Capital Region District (All)':
+		k = current_df[["search_neighbourhood", "sentiment"]]
+		k_pivot = k.pivot_table(index='search_neighbourhood',
 									columns='sentiment',
 									aggfunc=len,
 									fill_value=0)
-			st.write(k_pivot)
-		else:
-			st.sidebar.error("No options selected. Please select at least one location.")
+		st.write(k_pivot)
+	else:
+		st.sidebar.error("No options selected. Please select at least one location.")
 
 
-		if (len(locations_selected) > 0 or agg_option == 'Capital Region District (All)'):
-			# Need more twitter to determine how useful this will be
-			conditions = [
-				(current_df['sentiment'] == "Neutral"),
-				(current_df['sentiment'] == "Negative"),
-				(current_df['sentiment'] == "Positive")
-			]
-			values = [0, current_df["score"] * -1, current_df["score"]]
-			current_df["polarity"] = np.select(conditions, values)
-
-			j = current_df[["search_neighbourhood", "polarity", "created_at"]]
-			j['created_at'] = j['created_at'].dt.date
-			wide_form = j.pivot_table(index='created_at',
-									  columns='search_neighbourhood',
-									  values='polarity',
-									  aggfunc='mean',
-									  fill_value=0)
-
-			wide_form = wide_form.reset_index()
-			long_form = wide_form.melt('created_at', var_name='search_neighbourhood', value_name='avg_score')
-			line_chart = alt.Chart(long_form, title="Average Sentiment Scores by Date").mark_line().encode(
-				x='yearmonthdate(created_at)',
-				y='avg_score:Q',
-				tooltip='search_neighbourhood',
-				color='search_neighbourhood:N'
-			).interactive()
-			st.altair_chart(line_chart, use_container_width=True)
 
 
-		# 	real_data = gvceh.get_lat_long(k)
-		# 	st.pydeck_chart(pdk.Deck(
-		# 		map_style='mapbox://styles/mapbox/light-v9',
-		# 		initial_view_state=pdk.ViewState(
-		# 			latitude=48.45,
-		# 			longitude=-123.37,
-		# 			zoom=11,
-		# 			pitch=50,
-		# 			tooltip=True,
-		# 		),
-		# 		tooltip={
-		# 			'html': "<b>Number of Tweets:</b> {elevationValue}",
-		# 			'style': {
-		# 				'color': 'white'
-		# 			}
-		# 		},
-		# 		layers=[
-		# 			pdk.Layer(
-		# 				'HexagonLayer',
-		# 				twitter=real_data,
-		# 				get_position='[Longitude, Latitude]',
-		# 				auto_highlight=True,
-		# 				elevation_scale=4,
-		# 				radius=200,
-		# 				pickable=True,
-		# 				elevation_range=[10, 100],
-		# 				get_fill_color=[69, 162, 128, 255],
-		# 				extruded=True,
-		# 				coverage=1,
-		# 			),
-		# 		],
-		# 	))
+
+
 
 
 
