190f57a11b2ea86a7a6ece7e57fca29d38f61d21
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-07-18 16:36:09 -0700

    Create test.py

---
+++ scraper/test.py
@@ -0,0 +1,34 @@
+import datetime
+import os
+import pickle
+import os.path
+import time
+import model  # gvceh functions
+import cleaner  # gvceh functions
+import tweepy as tw
+import pandas as pd
+import io
+import requests
+
+from github import Github
+from transformers import pipeline
+
+from pprint import pprint
+import dotenv
+
+
+
+USERNAME = os.environ["USERNAME"]  # for github api
+TOKEN = os.environ["TOKEN"]  # for github api
+
+# open github api connection
+g = Github(USERNAME, TOKEN)
+user = g.get_user(USERNAME)
+repo = user.get_repo("SWB-GVCEH")
+
+
+print("READING CSV FROM GITHUB")
+consolidated_file_path = 'https://github.com/sheilaflood/SWB-GVCEH/blob/main/data/processed/twitter/GVCEH-tweets-combined.csv'
+s=requests.get(consolidated_file_path).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+print(c.head(5))
