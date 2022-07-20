a6719ff2324f3f32168222d6319a378f30c4f1eb
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-07-20 16:53:29 -0700

    Update test.py

--- scraper/test.py
+++ scraper/test.py
@@ -28,11 +28,20 @@ repo = user.get_repo("SWB-GVCEH")
 
 
 print("READING CSV FROM GITHUB")
-consolidated_file_path = 'https://github.com/sheilaflood/SWB-GVCEH/blob/main/data/processed/twitter/GVCEH-tweets-combined.csv'
-s=requests.get(consolidated_file_path).content
-c=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-print("Updating same file") 
-df_csv = c.to_csv()
-repo.update_file(consolidated_file_path, "Adding new tweets", df_csv, branch="main")
+consolidated_file_path = f"https://raw.githubusercontent.com/sheilaflood/SWB-GVCEH/main/data/processed/twitter/GVCEH-tweets-combined.csv"
+s=requests.get(consolidated_file_path).content
+df_old=pd.read_csv(io.StringIO(s.decode('utf-8')))
+print('Original CSV: ', df_old.shape)
+
+df_old = df_old[["text", "scrape_time", "tweet_id", "created_at", "reply_count", "quote_count",
+                    "like_count", "retweet_count", "geo_full_name", "geo_id", "username", "num_followers",
+                    "search_keywords", "search_neighbourhood", "sentiment", "score"]]
+    
+print(df_old.head())
+
+
+df_csv = df3.to_csv()
+
+repo.update_file(path = consolidated_file_path, message = "Adding new tweets", sha = df_csv.sha, branch="main", content = df_csv)
 print("Done with scraper.py!!!")
