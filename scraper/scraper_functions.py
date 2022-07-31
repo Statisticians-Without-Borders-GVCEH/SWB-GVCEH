5eabbc186f1150edddb3c177a2c9eb2b7a9f04d2
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-07-31 12:45:14 -0700

    Update scraper_functions.py

--- scraper/scraper_functions.py
+++ scraper/scraper_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import io
 from github import Github
+import base64
 
 
 def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
@@ -17,12 +18,15 @@ def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
     repo = user.get_repo("SWB-GVCEH")
 
     # Read CSV from Github -> Dataframe
-    # contents = repo.get_contents("")
     contents = repo.get_contents(git_file)
-    print(help(contents))
 
-    stuff = contents.decoded_content
-    df_old = pd.read_csv(io.StringIO(stuff.decode('utf-8')))
+    ## contents.decode doesn't work if file > 1 MB
+    # stuff = contents.decoded_content
+    # df_old = pd.read_csv(io.StringIO(contents.decode('utf-8')))
+    blob = repo.get_git_blob(contents.sha)
+    b64 = base64.b64decode(blob.content)
+    b64_decoded = b64.decode("utf8")
+    df_old = pd.read_csv(io.StringIO(b64_decoded))
 
     df_old = df_old[["text", "scrape_time", "tweet_id", "created_at", "reply_count", "quote_count",
              "like_count", "retweet_count", "geo_full_name", "geo_id", "username", "num_followers",
