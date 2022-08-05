7623db514f15c4f6a9443328a0999724266a1846
sheilaflood <71520769+sheilaflood@users.noreply.github.com>
2022-08-04 23:18:23 -0700

    Update scraper_functions.py

--- scraper/scraper_functions.py
+++ scraper/scraper_functions.py
@@ -38,10 +38,6 @@ def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
     df_old = pd.read_csv(io.StringIO(b64_decoded))
     df_old = df_old[columns]
     print('Current CSV: ', df_old.shape)
-#     df_old['tweet_id'] = pd.to_numeric(df_old['tweet_id'], errors='coerce') #TODO: this drops all non-numeric tweetID rows; need to find what's causingt his error
-#     df_old = df_old.dropna(subset=['tweet_id'])
-#     df_old['tweet_id'] = df_old['tweet_id'].astype('int')
-#     print('Current CSV after invalid rows: ', df_old.shape)
 
     if last_file_MB > 1:
         print("Latest file size exceeds limit - creating new csv...")
@@ -50,8 +46,6 @@ def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
         df_merged = pd.concat([df_old, df_new]).drop_duplicates(subset='tweet_id', keep=False).reset_index(drop=True)
         df_merged = pd.concat([df_old, df_merged]).drop_duplicates(subset='tweet_id', keep=False).reset_index(drop=True)
         
-#         different_cols = df_new.columns.difference(df_old.columns)
-#         df_merged = pd.merge(df_old, df_new[different_cols], how='right', on='tweet_id').reset_index(drop=True)
 
         # dedupe new data against second to last file in repo; don't keep any duplicates
         contents = repo.get_contents(second_to_last_file.path)
@@ -59,15 +53,10 @@ def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
         b64_decoded = base64.b64decode(blob.content).decode("utf8")
         df_old = pd.read_csv(io.StringIO(b64_decoded))
         df_old = df_old[columns]
-#         df_old['tweet_id'] = pd.to_numeric(df_old['tweet_id'], errors='coerce') #TODO: this drops all non-numeric tweetID rows; need to find what's causingt his error
-#         df_old = df_old.dropna(subset=['tweet_id'])
-#         df_old['tweet_id'] = df_old['tweet_id'].astype('int')
         print('Current CSV after invalid rows: ', df_old.shape)
         
         df_merged = pd.concat([df_old, df_merged]).drop_duplicates(subset='tweet_id', keep=False).reset_index(drop=True)
         df_merged = pd.concat([df_old, df_merged]).drop_duplicates(subset='tweet_id', keep=False).reset_index(drop=True)
-#         different_cols = df_new.columns.difference(df_old.columns)
-#         df_merged = pd.merge(df_old, df_merged[different_cols], how='right', on='tweet_id').reset_index(drop=True)
         
         print('New Unique Tweets: ', df_merged.shape)
 
@@ -85,52 +74,5 @@ def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
         repo.update_file(path=last_file.path, message="Adding new tweets", sha=contents.sha, branch="main",
                          content=df_csv)
         print(f"{last_file} updated in Github!")
-
-
-"""
-def update_file_in_github(USERNAME, TOKEN, git_file, df_new):
-
-    df_new = df_new[["text", "scrape_time", "tweet_id", "created_at", "reply_count", "quote_count",
-                     "like_count", "retweet_count", "geo_full_name", "geo_id", "username", "num_followers",
-                     "search_keywords", "search_neighbourhood", "sentiment", "score"]]
-
-    print('New Tweets: ', df_new.shape)
-
-    # open github api connection
-    g = Github(USERNAME, TOKEN)
-    user = g.get_user(USERNAME)
-    repo = user.get_repo("SWB-GVCEH")
-
-    # Read CSV from Github -> Dataframe
-    contents = repo.get_contents(git_file)
-
-    ## contents.decode doesn't work if file > 1 MB
-    # stuff = contents.decoded_content
-    # df_old = pd.read_csv(io.StringIO(contents.decode('utf-8')))
-    blob = repo.get_git_blob(contents.sha)
-    b64 = base64.b64decode(blob.content)
-    b64_decoded = b64.decode("utf8")
-    df_old = pd.read_csv(io.StringIO(b64_decoded))
-
-    df_old = df_old[["text", "scrape_time", "tweet_id", "created_at", "reply_count", "quote_count",
-             "like_count", "retweet_count", "geo_full_name", "geo_id", "username", "num_followers",
-             "search_keywords", "search_neighbourhood", "sentiment", "score"]]
-    print('Original CSV: ', df_old.shape)
-
-    df_merged = pd.concat([df_old, df_new]).drop_duplicates(subset='tweet_id', keep="last").reset_index(drop=True)
-    print('Combined CSVs: ', df_merged.shape)
-
-
-    # Convert DF back to CSV
-    df_csv = df_merged.to_csv()
-
-    repo.update_file(path=git_file, message="Adding new tweets", sha=contents.sha, branch="main", content=df_csv)
-
-    print("File Updated in Github!")
-"""
-
-
-
-
 
 
